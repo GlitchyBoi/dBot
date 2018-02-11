bdf5c8a2e140bb4af2fdadcec4c6153c2aaa82d0
shayandoust <shayan.doust@outlook.com>
2018-02-11 17:22:14 +0000

    Commit

--- dBot/Program.cs
+++ dBot/Program.cs
@@ -33,7 +33,7 @@ namespace dBot
             });
             client.Log += Logger;
             commands.Log += Logger;
-            // Read JSON data
+            // Read JSON data 
             ReadJson();
         }
         private void ReadJson()
